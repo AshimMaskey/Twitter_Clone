496cf98e15f24325ba52f79afcbf8c86ac314ba7
Ashim Maskey <ashimmaskey4@gmail.com>
2025-04-09 17:08:37 +0545

    added protectRoute middleware & getMe controller

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized: No token Provided" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedToken) {
+      return res.status(401).json({ error: "Unauthorized: No token Provided" });
+    }
+
+    const user = await User.findById(decodedToken.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import dbconnect from "./utils/dbconnect.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 
 //middleware
 app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse from data(urlencooded)
+app.use(cookieParser());
 
 //routes
 app.use("/api/auth", authRoutes);
