4d2fe272719e51ed6baa137105a47eae1e609886
Ashim Maskey <ashimmaskey4@gmail.com>
2025-04-12 17:14:02 +0545

    added updateuser & getsuggesteduser controller, cloudinary file upload

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,20 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import dbconnect from "./utils/dbconnect.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
 //middleware
 app.use(express.json()); //to parse req.body
-app.use(express.urlencoded({ extended: true })); // to parse from data(urlencooded)
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencooded)
 app.use(cookieParser());
 
 //routes
